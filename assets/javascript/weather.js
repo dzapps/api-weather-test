2b665f2ab28a8ef0a9f6cd7c99a98f6aec508cce
caperez <caperez@blizzard.com>
2017-04-18 15:22:33 -0700

    added the full js. Now I need to wait 60min. to get authorized

---
+++ assets/javascript/weather.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    var appID = "api.openweathermap.org/data/2.5/forecast?id=524901&APPID=2062afb45e93fb2d3ecbec522afac3ef";
+
+    $(".query_btn").click(function(){
+        var query_param = $(this).prev().val();
+
+        if ($(this).prev().attr("placeholder") == "City") {
+            var weather = "http://api.openweathermap.org/data/2.5/weather?q=" + query_param + "&APPID=" + appID;
+        } else if ($(this).prev().attr("placeholder") == "Zip Code") {
+            var weather = "http://api.openweathermap.org/data/2.5/weather?zip=" + query_param + "&APPID=" + appID;
+        }
+
+        $.getJSON(weather,function(json){
+            $("#city").html(json.name);
+            $("#main_weather").html(json.weather[0].main);
+            $("#description_weather").html(json.weather[0].description);
+            $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
+            $("#temperature").html(json.main.temp);
+            $("#pressure").html(json.main.pressure);
+            $("#humidity").html(json.main.humidity);
+        })
+    })
+});
